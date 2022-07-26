0a436f9ad23844cb043b7bfc69bf6aa1f8b27b15
Brandon Thread <negativegravitymusic@gmail.com>
2022-07-25 22:11:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/timeline-tool/',
   resolve: {
     alias: {
       '@c': path.resolve(__dirname, './src/components'),
